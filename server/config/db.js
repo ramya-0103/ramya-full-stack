4e22b0ca47f522ac91bb0f47d88ad510cccfd10e
ramya-0103 <ramyasri.010305@gmail.com>
2024-11-10 20:47:14 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Connect to MongoDB using the connection string from .env file
     await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB connected!");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error("Error connecting to MongoDB: ", err.message);
+    process.exit(1); // Exit the process if connection fails
   }
 };
 
 module.exports = connectDB;
+
