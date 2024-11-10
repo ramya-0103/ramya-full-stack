7e1307c9c4081c9abdc2258fdeb4fea2b8acfc74
ramya-0103 <ramyasri.010305@gmail.com>
2024-11-10 20:51:09 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,8 +4,10 @@ const { saveArticleData } = require("../controllers/articleDataController");
 
 const router = express.Router();
 
+// Route for saving search value
 router.post("/searchValue", saveSearchValue);
 
+// Route for saving clicked article data
 router.post("/clickedArticle", saveArticleData);
 
 module.exports = router;
